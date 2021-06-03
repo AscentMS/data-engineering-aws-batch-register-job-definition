531f3176027522130db1d6ce1f517534f3fc868e
Jonathan Stott <jonathan.stott@evergreen-life.co.uk>
2021-06-03 16:32:27 +0100

    Add working basic test

---
+++ index.test.js
@@ -0,0 +1,70 @@
+const run = require('.');
+const core = require('@actions/core');
+const fs = require('fs');
+const path = require('path');
+
+jest.mock('@actions/core');
+jest.mock('fs');
+
+const mockBatchRegisterJobDef = jest.fn();
+jest.mock('aws-sdk', () => {
+    return {
+        config: {
+            region: 'fake-region'
+        },
+        Batch: jest.fn(() => ({
+            registerJobDefinition: mockBatchRegisterJobDef,
+        })),
+    };
+});
+
+
+describe('Deploy to ECS', () => {
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        core.getInput = jest
+            .fn()
+            .mockReturnValueOnce('job-definition.json'); // job-definition
+
+        process.env = Object.assign(process.env, { GITHUB_WORKSPACE: __dirname });
+
+        fs.existsSync.mockReturnValue(true);
+
+        jest.mock('./job-definition.json', () => ({
+            type: 'container',
+            containerProperties: {
+                image: "some-other-image"
+            }
+        }),
+            { virtual: true });
+
+        mockBatchRegisterJobDef.mockImplementation(() => {
+            return {
+                promise() {
+                    return Promise.resolve({
+                        jobDefinitionArn: 'job:def:arn',
+                        jobDefinitionName: 'job',
+                        revision: 1,
+                    }
+                    );
+                }
+            };
+        });
+    });
+
+    test('registers the job definition contents', async () => {
+        await run();
+        expect(core.setFailed).toHaveBeenCalledTimes(0);
+        expect(mockBatchRegisterJobDef).toHaveBeenNthCalledWith(1, {
+            type: 'container',
+            containerProperties: {
+                image: "some-other-image"
+            }
+        });
+        expect(core.setOutput).toHaveBeenNthCalledWith(1, 'job-definition-arn', 'job:def:arn');
+        expect(core.setOutput).toHaveBeenNthCalledWith(2, 'job-definition-name', 'job');
+        expect(core.setOutput).toHaveBeenNthCalledWith(3, 'revision', 1);
+    });
+})
